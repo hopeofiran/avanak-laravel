ab15fbda9cea297da832a794a3b595469a0a6a0a
OmidMehri <thehopeofiran@gmail.com>
2022-09-06 16:40:37 +0430

    added avanak config file

---
+++ src/config/avanak.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'baseUrl'  => 'https://portal.avanak.ir/webservice3.asmx?WSDL',
+    'username' => env('AVANAK_USERNAME', ''),
+    'password' => env('AVANAK_PASSWORD', ''),
+];
